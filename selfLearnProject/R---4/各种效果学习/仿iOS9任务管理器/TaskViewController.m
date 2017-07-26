668c2340a008d6924f9f01a4837060932c468d12
huamanlou <15810427253@163.com>
2017-07-26 16:46:59 +0800

    仿iOS9+任务管理器界面效果

--- selfLearnProject/R---4/各种效果学习/仿iOS9任务管理器/TaskViewController.m
+++ selfLearnProject/R---4/各种效果学习/仿iOS9任务管理器/TaskViewController.m
@@ -36,7 +36,7 @@
 
 -(NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
 {
-    return 10;
+    return 8;
 }
 
 
@@ -51,6 +51,11 @@
         UIImageView * imageView =[[UIImageView alloc]initWithFrame:taskView.bounds];
         [imageView setContentMode:UIViewContentModeScaleAspectFill];
         [imageView setBackgroundColor:[UIColor whiteColor]];
+        
+        UIImage * taskImage = [UIImage imageNamed:[NSString stringWithFormat:@"%ld",index]];
+        
+        [imageView setImage: taskImage];
+        
         UILabel * label = [[UILabel alloc]initWithFrame:taskView.frame];
         [label setText:[@(index) stringValue]];
         [label setFont:[UIFont systemFontOfSize:50]];
@@ -69,16 +74,34 @@
         [layer setPath:[UIBezierPath bezierPathWithRoundedRect:imageView.frame cornerRadius:5.0f].CGPath];
         [imageView.layer setMask:layer];
         
-        [imageView addSubview:label];
+        //[imageView addSubview:label];
         [taskView addSubview:imageView];
     }
     
     return taskView;
 }
+
+//计算缩放
+-(CGFloat)calcSacalewithOffset:(CGFloat)offset{
+    return  offset * 0.02f +1.0f;
+}
+
+//计算位移
+-(CGFloat)calcTranslationWithOffset:(CGFloat)offset{
+    CGFloat z = 5.0f/4.0f;
+    CGFloat a = 5.0f/8.0f;
+    if (offset >= z/a) {
+        return 2.0;
+    }
+    return 1/(z-a*offset)-1/z;
+}
 -(CATransform3D)carousel:(iCarousel *)carousel itemTransformForOffset:(CGFloat)offset baseTransform:(CATransform3D)transform
 {
     
-    return CATransform3DTranslate(transform, offset*self.tasksize.width, 0, 0);
+    CGFloat scale =[self calcSacalewithOffset:offset];
+    CGFloat translation = [self calcTranslationWithOffset:offset];
+    
+    return CATransform3DScale(CATransform3DTranslate(transform, translation*self.tasksize.width, 0, offset), scale, scale, 1.0);
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
